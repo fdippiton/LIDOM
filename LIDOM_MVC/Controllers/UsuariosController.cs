9161f5c5455212676ff16d8f191b8562473dc600
Manuel Matos <ma.mgrd20@gmail.com>
2023-10-30 20:17:24 -0400

    new changes

--- LIDOM_MVC/Controllers/UsuariosController.cs
+++ LIDOM_MVC/Controllers/UsuariosController.cs
@@ -1,14 +1,79 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LIDOM_MVC.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Globalization;
 
 namespace LIDOM_MVC.Controllers
 {
     public class UsuariosController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient httpClient;
+
+        public UsuariosController(IConfiguration configuration)
+        {
+            httpClient = new HttpClient();
+            _configuration = configuration;
+        }
+
         // GET: UsuariosController
-        public ActionResult Index()
+        public ActionResult Index() // login form
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AuthUser(string us, string pass)
+        {
+            Usuario userAuth = null;
+            using (var client = new HttpClient())
+            {
+                string baseApiUrl = _configuration.GetSection("LigaDominicanaApi").Value;
+                client.BaseAddress = new Uri(baseApiUrl);
+                string apiUrl = $"/O?us={us}&pass={pass}";
+                var response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var dataUser = response.Content.ReadAsStringAsync().Result;
+                    userAuth = JsonConvert.DeserializeObject<Usuario>(dataUser);
+
+                    if (userAuth != null)
+                    {
+                        if (userAuth.UsuRol == 1)
+                        {
+                            // Autenticación exitosa
+                            var claims = new List<Claim>
+                            {
+                                new Claim(ClaimTypes.Name, userAuth.UsuNombre),
+                                new Claim(ClaimTypes.Role, "Admin")
+                            };
+
+                            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+                        }
+                 
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Usuario o contraseña incorrectos");
+            return View(userAuth);
+        }
+
+        public async Task<ActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
         }
 
         // GET: UsuariosController/Details/5
